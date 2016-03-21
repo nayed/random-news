20fa62cb756388439b428af818c16c0d0c41aa4f
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-03-21 20:58:34 +0100

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
-/**
- * Webpack config for development
- */
-module.exports = require('./webpack.make')({
-  BUILD: false,
-  TEST: false
-})
+var path = require('path')
+var node_modules = path.resolve(__dirname, 'node_modules')
+var webpack = require('webpack')
+
+config = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/app.js')],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel'
+        }, {
+            test: /\.css$/, // Only .css files
+            loader: 'style!css' // Run both loaders
+        }]
+    }
+}
+
+module.exports = config
