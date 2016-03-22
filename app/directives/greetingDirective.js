f3288445b21dd0c700afe196763ffe9d6de08b28
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-03-22 16:50:54 +0100

    testing directives in ES6

---
+++ app/directives/greetingDirective.js
@@ -0,0 +1,15 @@
+import angular from 'angular'
+
+function greeting() {
+    return {
+        restrict: 'E',
+        scope: {
+            name: '='
+        },
+        template: '<h1>Hello {{name}}</h1>'
+    }
+}
+
+export default angular.module('directives.greeting', [])
+    .directive('greeting', greeting)
+    .name
--- app/features/home/index.js
+++ app/features/home/index.js
@@ -4,10 +4,11 @@ import uirouter from 'angular-ui-router'
 import routing from './homeRoutes'
 import {HomeController} from './homeController'
 import randomNames from '../../services/randomNamesService'
+import greeting from '../../directives/greetingDirective'
 
 import './home.css'
 
-export default angular.module('appHome', [uirouter, randomNames])
+export default angular.module('appHome', [uirouter, randomNames, greeting])
     .config(routing)
     .controller('HomeController', HomeController)
     .name
