9da3a92fea765db6fc47dd507e213f863ea739a8
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-03-31 13:35:34 +0200

    finished

--- app/directives/greetingDirective.js
+++ app/directives/greetingDirective.js
@@ -6,7 +6,11 @@ function greeting() {
         scope: {
             post: '='
         },
-        template: '<h5>{{post}}</h1>'
+        template: `
+            <h1>{{post.title}}</h1>
+            <h3><a href="{{post.url}}" target="_blank">{{post.url}}</a></h3>
+            <h5 ng-if="post.objectID"><a href="https://news.ycombinator.com/item?id={{post.objectID}}" target="_blank">Hacker News Thread</a></h5>
+        `
     }
 }
 
